97addf5afb392e07daa3b418bafe3c32e9d4fa9c
Lương Thiện <luongthiendn1@gmail.com>
2022-12-24 12:49:48 +0700

    port

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ $password = "";
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect('localhost', 'root', '', 'onlineshop2', 3307);
-$db = new PDO('mysql:host=localhost; port=3307;dbname=onlineshop', 'root', '');
+$con = mysqli_connect('localhost', 'root', '', 'onlineshop2', 3306);
+$db = new PDO('mysql:host=localhost; port=3306;dbname=onlineshop', 'root', '');
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
